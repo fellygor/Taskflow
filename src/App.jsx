4d2f0d64b764399833b0420b9981f6d2b333bc6e
unknown <fellygor@gmail.com>
2026-09-18 22:36:31 +0300

    Fix: Updated routed in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/auth' element={<AuthScreen />} />
+        <Route path='/' element={<AuthScreen />} />
         <Route 
           path='/dashboard' 
           element={
